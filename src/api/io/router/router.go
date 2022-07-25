513795e9f3c1baa19351550d54ddee846959e357
Rodrigo López <rodri.lopez98@gmail.com>
2022-07-25 16:10:21 -0300

    Fix db connection string

--- src/api/io/router/router.go
+++ src/api/io/router/router.go
@@ -33,7 +33,7 @@ func mapHandlers(env environment.Env) {
 func SetupRunEnv(env environment.Env) {
 	log.Print("[package:router] Configuring routes...")
 	ConfigureRoute(env)
-	log.Printf(fmt.Sprintf("[package:router] Listening on routes: %mocks", pingPath))
+	log.Printf(fmt.Sprintf("[package:router] Listening on routes: %s, %s", pingPath, userPath))
 }
 
 func ConfigureRoute(env environment.Env) *gin.Engine {
