2413d002a1b825dc473a94833d7bc1b8611b45c1
Juan Andres Gossweiler <juan.gossweiler@mercadolibre.com>
2022-08-30 19:53:18 -0300

    dog imgs in register and update

--- src/api/usecases/interfaces/dog_manager.go
+++ src/api/usecases/interfaces/dog_manager.go
@@ -4,7 +4,7 @@ import "be-tpms/src/api/domain/model"
 
 type DogManager interface {
 	Get(uint) (*model.Dog, error)
-	Register(*model.Dog) (*model.Dog, error)
-	Modify(*model.Dog) (*model.Dog, error)
+	Register(*model.Dog, [][]byte) (*model.Dog, error)
+	Modify(*model.Dog, [][]byte) (*model.Dog, error)
 	Delete(uint) (bool, error)
 }
