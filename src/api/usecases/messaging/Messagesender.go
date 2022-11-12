67531787054ed464f5a61a072f0182e36c40de6f
Juan Andres Gossweiler <juan.agossweiler@gmail.com>
2022-11-12 17:30:56 -0300

    fix missing value

--- src/api/usecases/messaging/Messagesender.go
+++ src/api/usecases/messaging/Messagesender.go
@@ -2,7 +2,9 @@ package messaging
 
 import (
 	"be-tpms/src/api/domain/model"
+	"be-tpms/src/api/io"
 	"be-tpms/src/api/usecases/interfaces"
+	"fmt"
 	"log"
 )
 
@@ -18,7 +20,19 @@ func NewMessageSender(messaging interfaces.Messaging, persister interfaces.UserP
 	}
 }
 
-func (ms *MessageSender) SendToEnabledUsers(reporterUserID string, data map[string]string) error {
+func (ms *MessageSender) SendToEnabledUsers(dog *model.Dog) error {
+	data := map[string]string{
+		io.TITLE: "Se ha perdido un perro cerca tuyo!",
+		io.BODY:  fmt.Sprintf("Se perdio un perro de raza %s cerca tuyo!", dog.Breed.String()),
+	}
+
+	var reporterUserID string
+	if dog.Owner == nil {
+		reporterUserID = dog.Host.ID
+	} else {
+		reporterUserID = dog.Owner.ID
+	}
+
 	possibleUsers, err := ms.userPersister.GetUsersEnabledMessages()
 	if err != nil {
 		return err
