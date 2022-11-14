f614e57b6c14ec7dbb59a4577d4992155d2ff09d
Juan Andres Gossweiler <juan.agossweiler@gmail.com>
2022-11-14 02:16:05 -0300

    delete interfaces

--- src/api/domain/model/dog.go
+++ src/api/domain/model/dog.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql/driver"
 	"strings"
 	"time"
 )
@@ -1093,6 +1092,3 @@ func ParseAck(ack string) Ack {
 		return Pending
 	}
 }
-
-func (a *Ack) Scan(value interface{}) error { *a = Ack(value.(int8)); return nil }
-func (a Ack) Value() (driver.Value, error)  { return int8(a), nil }
